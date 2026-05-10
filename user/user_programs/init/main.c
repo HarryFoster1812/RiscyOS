5dd08dbe739c224c9f8175ec958e4b94a6bab6f9
r49769hf <harry.foster-2@student.manchester.ac.uk>
2026-05-10 23:50:04 +0100

    added init and a simple hello program

---
+++ user/user_programs/init/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+	// Fork
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid == 0) {
+		execv("/shell", NULL);
+		exit(1);
+	}             // Parent
+	while (1) {
+		wait(NULL);
+	}
+
+	return 0;
+}
